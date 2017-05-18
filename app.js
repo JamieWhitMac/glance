8b394617cc1977a8f7a0fe9931969b43b8e5c351
JamieWhitMac <jamie.whit.mac@gmail.com>
2017-05-18 10:53:05 +0100

    Deployment test 2

--- app.js
+++ app.js
@@ -41,12 +41,13 @@ app.use("/users", users);
 
 // Index route
 app.get("/", (req, res) => {
-    res.send("Hello there.");
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+   // res.send("Hello there.");
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname, 'public/index.html'));
+//});
 
 app.listen (port, () =>{
     console.log("Server started on port "+port);
